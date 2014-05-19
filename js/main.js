82dfadbd35817f091d3b5b79d2234b94b57f0f48
chris7197 <Chris@Chriss-MacBook-Pro.local>
2014-05-19 22:37:49 +0100

    h

--- js/main.js
+++ js/main.js
@@ -10,7 +10,6 @@ var app = {
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
         document.getElementById('scan').addEventListener('click', this.scan, false);
-        document.getElementById('encode').addEventListener('click', this.encode, false);
     },
 
     // deviceready Event Handler
